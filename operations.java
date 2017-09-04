f91389d2c6d6ab6d37747808186ab8b15f84ac88
nitin radke <nitinirctc@gmail.com>
2017-09-04 16:06:42 +0530

    java files 04/09/17

---
+++ operations.java
@@ -0,0 +1,19 @@
+package p1.p2;
+public class operations
+{
+	public void multiply(int a,int b)
+	{
+		System.out.println(a*b);
+	}
+	
+	public void subtract(int a,int b)
+	{
+		System.out.println(a-b);
+	}
+	
+	public void divide(int a,int b)
+	{
+		System.out.println(a/b);
+	}
+}
+	
