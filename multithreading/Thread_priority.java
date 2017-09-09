04abe22ca0535f8069e9547fc3e3886f6fccc2f5
nitin <nitinradkeirctc@gmail.com>
2017-09-09 20:56:15 +0530

    added multithreading

--- multithreading/Thread_priority.java
+++ multithreading/Thread_priority.java
@@ -2,10 +2,12 @@ class Mythread extends Thread
 {
 
 }
+
 class Thread_priority{
   public static void main(String [] args)
   {
-    //Thread.currentThread().setPriority(7);
+    Thread.currentThread().setPriority(7);  //only default priority of main thread is 5
+					   //other thread just inherits priority freom parents
     Mythread t1=new Mythread();
     System.out.println(t1.getPriority());
   }
