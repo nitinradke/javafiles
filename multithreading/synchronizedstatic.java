0f01329116aefaac5d3acfe46263f229a3fb33fb
nitin <nitinradkeirctc@gmail.com>
2017-09-12 00:59:33 +0530

    sycronize with static

---
+++ multithreading/synchronizedstatic.java
@@ -0,0 +1,45 @@
+class Synchronized
+{
+	public static void main(String [] args)
+	{
+	Display d=new Display();
+	Display d1=new Display();
+	mythread m1=new mythread(d,"Dhoni");
+	mythread m2=new mythread(d1,"Yuvraj");
+	m1.start();
+	m2.start();
+	}
+}
+
+class mythread extends Thread
+{
+	Display d;
+	String name;
+	mythread(Display d,String name)
+	{
+		this.d=d;
+		this.name=name;
+	}
+	public void run()
+	{
+		d.wish(name);
+	}
+}
+
+class Display
+{
+	static synchronized void wish(String s)
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(s+"Batting");
+			try
+			{
+				Thread.sleep(10);
+			}
+			catch(InterruptedException ie)
+			{}
+		}
+	}
+}
+
